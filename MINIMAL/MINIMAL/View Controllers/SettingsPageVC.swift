1a3e42a3c4e49318b3f5bd1856a3d14fe81c7fcc
Gopi69 <143544377+Gopi69@users.noreply.github.com>
2023-10-24 21:56:22 -0500

    Added Screen for Settings

---
+++ MINIMAL/MINIMAL/View Controllers/SettingsPageVC.swift
@@ -0,0 +1,40 @@
+//
+//  SettingsViewController.swift
+//  MINIMAL
+//
+//  Created by Chintu Chowdary on 10/24/23.
+//
+
+import UIKit
+
+class SettingsPageVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var settingsTableView: UITableView!
+    
+    private var settingsList = ["Account", "Notifications", "Appearance", "Privacy & Security", "Help & Support", "About"]
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        settingsTableView.dataSource = self
+        settingsTableView.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settingsList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let setting = settingsList[indexPath.row]
+        let settingsCell = settingsTableView.dequeueReusableCell(withIdentifier: "settingsCell", for: indexPath)
+        
+        settingsCell.textLabel?.text = setting
+        settingsCell.imageView?.image = UIImage(named: setting)
+        
+        return settingsCell
+    }
+
+}
